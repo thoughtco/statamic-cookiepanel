d594b3e3c1715a40f33545b51b6e5fbbe01e9ee1
Andrew Topping <andrew@thoughtcollective.com>
2023-06-13 11:52:48 +0100

    Update Cookie Panel for Laravel 9+

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,21 +7,22 @@ use Statamic;
 use Statamic\Facades\File;
 use Statamic\Facades\YAML;
 use Statamic\Providers\AddonServiceProvider;
+use Thoughtco\CookiePanel\Http\Controllers\CookiePanelController;
 
 class ServiceProvider extends AddonServiceProvider
 {
     protected $tags = [
         Tags\CookiePanel::class,
     ];
-    
+
     protected $scripts = [
         __DIR__.'/../resources/js/cookiepanel.js'
     ];
-    
+
     protected $stylesheets = [
         __DIR__.'/../resources/css/cookiepanel.css'
     ];
-    
+
     public function boot()
     {
         parent::boot();
@@ -29,24 +30,24 @@ class ServiceProvider extends AddonServiceProvider
         // handle the posting of the cookie
         Statamic::booted(function () {
             $this->registerActionRoutes(function () {
-                Route::post('/', 'CookiePanelController@index');
+                Route::post('/', [CookiePanelController::class, 'index']);
             });
         });
-    
+
         // after install we need to copy our global
         Statamic::afterInstalled(function ($command) {
-            
+
             if (File::exists(resource_path('blueprints/globals/cookie_panel.yaml')))
                 return;
-            
+
             $original = __DIR__.'/../resources/blueprints/cookie_panel.yaml';
             $yaml = YAML::file($original)->parse();
             File::put(resource_path('blueprints/globals/cookie_panel.yaml'), YAML::dump($yaml));
-            
+
             $original = __DIR__.'/../resources/content/cookie_panel.yaml';
             $yaml = YAML::file($original)->parse();
             File::put(base_path('content/globals/cookie_panel.yaml'), YAML::dump($yaml));
-            
+
         });
     }
 }
